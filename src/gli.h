6f04f2dd7e7d597d1f5b8118c8f1dcdaef466dc5
James Delph <james.delph@hotmail.co.uk>
2017-02-17 16:49:43 +0000

    fixed windows includes, ugh

--- src/gli.h
+++ src/gli.h
@@ -1,6 +1,17 @@
 #ifdef _WIN32
+// reduce the crud we get from windows
+#define WIN32_LEAN_AND_MEAN
 #include <windows.h>
-#include "external/GL/freeglut.h"
+// windows.h is scrubby and probably just defined macros for max and min
+// however, this breaks the C++ versions provided by <algorithm>
+// so undefine them.
+#ifdef max
+#undef max
+#endif
+#ifdef min
+#undef min
+#endif
+#include "../external/GL/freeglut.h"
 #else
 #include <GL/gl.h>
 #include <GL/freeglut.h>
