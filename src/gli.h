3c8c4951f047a50542be0ea57e5ddc475de4b046
JeffBobbo <james.delph@hotmail.co.uk>
2017-02-17 22:28:08 +0000

    use NOMINMAX to prevent the definition of min/max instead of undeffing the macros

--- src/gli.h
+++ src/gli.h
@@ -1,16 +1,9 @@
 #ifdef _WIN32
 // reduce the crud we get from windows
 #define WIN32_LEAN_AND_MEAN
+// we don't want min and max macros from windows.h (ugh... why)
+#define NOMINMAX
 #include <windows.h>
-// windows.h is scrubby and probably just defined macros for max and min
-// however, this breaks the C++ versions provided by <algorithm>
-// so undefine them.
-#ifdef max
-#undef max
-#endif
-#ifdef min
-#undef min
-#endif
 #include "../external/GL/freeglut.h"
 #else
 #include <GL/gl.h>
