7e63d54cb80aa7b650850c639ea6016075530b82
JeffBobbo <james.delph@hotmail.co.uk>
2017-05-07 21:40:33 +0100

    camera/turret visuals

--- src/object/ai.cpp
+++ src/object/ai.cpp
@@ -57,8 +57,8 @@ void AI::move()
 Camera::Camera(double a, double b) : AI(0.4, 0.1, a, b), turnRate(60.0)
 {
   delete visage;
-  visage = VisagePolygon::rectangle(0.4, 0.1);
-  static_cast<VisagePolygon*>(visage)->setColour(0xAFAFAFFF);
+  visage = new VisageTexture(0.4, 0.1, "img/camera.png");
+  static_cast<VisageTexture*>(visage)->setAtlasSprite("standby");
   angle = 0.0;
 }
 
@@ -78,16 +78,27 @@ void Camera::move()
     return;
 
   if (hitScan(target) != target)
+  {
+    static_cast<VisageTexture*>(visage)->setAtlasSprite("standby");
     return;
+  }
+
 
   double d = degrees(angularDifference(angleTo(target), radians(angle)));
 
   double na = (d < 0.0 ? -1.0 : 1.0) * std::min(std::abs(d), turnRate * (delta / 1000.0));
   angle += na;
+  if (std::abs(angularDifference(angleTo(target), radians(angle))) < pi() / 6.0)
+    static_cast<VisageTexture*>(visage)->setAtlasSprite("active");
+  else
+    static_cast<VisageTexture*>(visage)->setAtlasSprite("standby");
 }
 
 Turret::Turret(double a, double b) : Camera(a, b)
 {
+  delete visage;
+  visage = new VisageTexture(1, 1, "img/turret.png");
+  static_cast<VisageTexture*>(visage)->setAtlasSprite("standby");
   lastFire = elapsed;
 }
 
@@ -103,16 +114,38 @@ void Turret::move()
   if (!target)
     return;
 
-  static const millis_t RECOIL = 100;
-  if (elapsed - lastFire > RECOIL && distance(target) < 10.0 && hitScan(target) == target && std::abs(angularDifference(angleTo(target), radians(angle))) < pi() / 6.0)
+  Object* const hit = hitScan(target);
+  const bool inArc = std::abs(angularDifference(angleTo(target), radians(angle))) < pi() / 6.0;
+
+  if (hit != target)
   {
-    static const double speed = 10.0;
-    const double a = angle * random(0.975, 1.025);
-    // fire!
-    Vec2D pos(x, y);
-    Vec2D vel(speed * std::cos(radians(a)), speed * std::sin(radians(a)));
-    Projectile* p = new Projectile(1, pos, vel, target, this);
-    lastFire = elapsed;
-    level->insert(SceneGraph::Level::NPC, p);
+    static_cast<VisageTexture*>(visage)->setAtlasSprite("standby");
+    return;
+  }
+
+  static const millis_t RECOIL = 100;
+
+  if (elapsed - lastFire > RECOIL)
+  {
+    if (distance(target) < 10.0 && inArc)
+    {
+      static const double speed = 10.0;
+      const double a = angle * random(0.975, 1.025);
+      // fire!
+      Vec2D pos(x + std::cos(radians(angle)) * 0.5, y + std::sin(radians(angle)) * 0.5);
+      Vec2D vel(speed * std::cos(radians(a)), speed * std::sin(radians(a)));
+      Projectile* p = new Projectile(1, pos, vel, target, this);
+      lastFire = elapsed;
+      level->insert(SceneGraph::Level::NPC, p);
+      static_cast<VisageTexture*>(visage)->setAtlasSprite("firing");
+    }
+    else
+    {
+      static_cast<VisageTexture*>(visage)->setAtlasSprite("active");
+    }
+  }
+  else
+  {
+    static_cast<VisageTexture*>(visage)->setAtlasSprite("firing");
   }
 }
